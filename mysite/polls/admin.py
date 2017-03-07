4a9422f863524976498103f7069a72c60787b71a
MyGb <ismybox@163.com>
2017-03-07 23:06:38 +0800

    commit about webApp code.

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,4 +1,10 @@
 from django.contrib import admin
-from .models import Question
+from .models import Question,Choice
 # Register your models here.
-admin.site.register(Question)
+# class QuestionAdmin(admin.ModelAdmin):
+#     fields = ['pub_date', 'question_text']
+
+class QuestionAdmin(admin.ModelAdmin):
+	fieldsets = [(None,{'fields': ['question_text']}),('Date information', {'fields': ['pub_date']}),]
+admin.site.register(Question,QuestionAdmin)
+admin.site.register(Choice)
