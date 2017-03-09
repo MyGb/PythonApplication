00726b1eecb08ac0e195644c5846a1f01cfad515
MyGb <ismybox@163.com>
2017-03-09 21:54:01 +0800

    return json format data.

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,9 +1,15 @@
 from django.http import HttpResponse,JsonResponse
 from django.core import serializers
 from .models import Job
+from django.db.models import Count
+import json
 # Create your views here.
 
 def index(request):
-	result = Job.objects.all().filter(financestage__contains="初创型")
-	data = serializers.serialize('json', result)
-	return HttpResponse(data)
+	#select industryfield,count(industryfield) from job group by industryfield
+	groupResult = Job.objects.values("industryfield").annotate(total=Count("industryfield"))
+	total = Job.objects.count()
+	info = (r for r in groupResult)
+	data = {"total":total,"info":list(info)}
+	jsonData = {"code":0,"message":"success","total":total,"data":data}
+	return HttpResponse(json.dumps(jsonData))
