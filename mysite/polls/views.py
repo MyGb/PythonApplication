afe4d9cc2be36b166496d3129456f5664a8b4003
huyw <hywbox@gmail.com>
2017-03-09 09:27:29 +0800

    add .gitignore file.

--- mysite/polls/views.py
+++
@@ -1,9 +0,0 @@
-from django.http import HttpResponse,JsonResponse
-from django.core import serializers
-from .models import Job
-# Create your views here.
-
-def index(request):
-	result = Job.objects.all().filter(financestage__contains="初创型")
-	data = serializers.serialize('json', result)
-	return HttpResponse(data)
