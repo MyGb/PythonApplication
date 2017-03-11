5d434a627c58c06c092b194a439ae574766ce894
MyGb <ismybox@163.com>
2017-03-11 19:31:39 +0800

    remove 'mysite' project.

--- webApp/api/views.py
+++ webApp/api/views.py
@@ -1,8 +1,14 @@
-from django.shortcuts import render
-
-# Create your views here.
 from django.http import HttpResponse
-
+from django.db.models import Count
+from .models import Job
+import json
+# Create your views here.
 
 def index(request):
-    return HttpResponse("Hello, world.")
+	#select industryfield,count(industryfield) from job group by industryfield
+	groupResult = Job.objects.values("industryfield").annotate(total=Count("industryfield"))
+	total = Job.objects.count()
+	info = (r for r in groupResult)
+	data = {"total":total,"info":list(info)}
+	jsonData = {"code":0,"message":"success","total":total,"data":data}
+	return HttpResponse(json.dumps(jsonData))
--- webApp/webApp/settings.py
+++ webApp/webApp/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'api.apps.ApiConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -76,7 +77,7 @@ WSGI_APPLICATION = 'webApp.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'python_test1',
+        'NAME': 'python_test',
         'USER': 'root',
         'PASSWORD': 'root',
         'HOST': '127.0.0.1',
